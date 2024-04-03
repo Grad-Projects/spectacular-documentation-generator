bf61ea3fd91d9322541095e22490c98e3d6d6860
Sean van Wyk <Sean.vanWyk@bbd.co.za>
2024-04-03 18:12:20 +0200

    OAuth dictionary parsing and error handling

--- OAuth/OAuthCLI/OAuth.cs
+++ OAuth/OAuthCLI/OAuth.cs
@@ -6,7 +6,7 @@ public class OAuth
     readonly string ClientID;
     readonly HttpClient client;
 
-    string? AccessToken { get; set; }
+    public string? AccessToken { get; set; }
     string _deviceCode = "";
 
     public OAuth(HttpClient client)
@@ -16,7 +16,7 @@ public class OAuth
         this.ClientID = envClientID;
     }
 
-    public async Task Login()
+    public async Task<bool> Login()
     {
         // Initialize
         var loginRequestDict = new Dictionary<string, string>
@@ -31,14 +31,12 @@ public class OAuth
         {
             var response = await client.PostAsync(loginURL, loginRequestBody);
 
-            var responseString = await response.Content.ReadAsStringAsync();
-            // TODO: Write a helper function to turn this into a dictionary
-            string[] responseSplit = responseString.Split("&");
-            _deviceCode = responseSplit[0].Split("=")[1];
-            interval = int.Parse(responseSplit[2].Split("=")[1]);
-            string loginCode = responseSplit[3].Split("=")[1];
-            var verificationURL = DecodeUrlString(responseSplit[4].Split("=")[1]);
-            System.Console.WriteLine($"Please navigate to {verificationURL}\nEnter the following Code: {loginCode}");
+            var responseDict = queryStringToDict(await response.Content.ReadAsStringAsync());
+            _deviceCode = responseDict["device_code"];
+            interval = int.Parse(responseDict["interval"]);
+            string userCode = responseDict["user_code"];
+            var verificationURL = DecodeUrlString(responseDict["verification_uri"]);
+            System.Console.WriteLine($"Please navigate to {verificationURL}\nEnter the following Code: {userCode}");
         }
         catch (HttpRequestException e)
         {
@@ -56,25 +54,39 @@ public class OAuth
         var pollRequestBody = new FormUrlEncodedContent(pollRequestDict);
         var pollURL = "https://github.com/login/oauth/access_token";
         bool verified = false;
-        int maxCount = 0;
-        while (!verified && maxCount < 10)
+        bool expired = false;
+        while (!verified && !expired)
         {
-            System.Console.WriteLine($"Polling #{maxCount}");
             Thread.Sleep((interval + 1) * 1000);
             try
             {
                 var response = await client.PostAsync(pollURL, pollRequestBody);
 
-                var responseString = await response.Content.ReadAsStringAsync();
-                string[] responseSplit = responseString.Split("&");
-                if (responseSplit[0].Split("=")[0] != "error")
+                var responseDict = queryStringToDict(await response.Content.ReadAsStringAsync());
+
+                if (responseDict.TryGetValue("access_token", out string? value))
                 {
                     verified = true;
-                    AccessToken = responseSplit[0].Split("=")[1];
+                    AccessToken = value;
                 }
                 else
                 {
-                    // TODO: Handle Errors
+                    var error = responseDict["error"];
+                    if (error == "authorization_pending")
+                    {
+                        // Do nothing, we're still waiting
+                    }
+                    else if (error == "slow_down")
+                    {
+                        interval = int.Parse(responseDict["interval"]);
+                    }
+                    else if (error == "expired_token")
+                    {
+                        expired = true;
+                    }
+                    else {
+                        throw new Exception($"Couldn't handle the error message. You must've set something up wrong!\nError was: {error}");
+                    }
                 }
             }
             catch (HttpRequestException e)
@@ -82,11 +94,9 @@ public class OAuth
                 Console.Error.WriteLine("Returned an error in the polling request! Catch me later in the CLI");
                 Console.Error.Write(e);
             }
-            maxCount++;
         }
 
-        System.Console.WriteLine($"Got Access Token: {AccessToken}");
-
+        return AccessToken != null;
     }
 
     // https://stackoverflow.com/a/3847593
@@ -96,5 +106,18 @@ public class OAuth
         while ((newUrl = Uri.UnescapeDataString(url)) != url)
             url = newUrl;
         return newUrl;
+    }
+
+    // Helper method to create a dictionary with the query string returned from GitHub
+    private static Dictionary<string, string> queryStringToDict(string queryString)
+    {
+        Dictionary<string, string> output = [];
+        string[] responseSplit = queryString.Split("&");
+        foreach (var s in responseSplit)
+        {
+            var sSplit = s.Split("=");
+            output.Add(sSplit[0], sSplit[1]);
+        }
+        return output;
     }
 }
