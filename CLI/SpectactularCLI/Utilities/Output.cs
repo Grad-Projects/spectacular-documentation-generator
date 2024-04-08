56543506d887a185b52243e0d0a08eea16f4ab34
BBDNET\bbdnet2874 <mcquile@bbd.co.za>
2024-04-08 15:33:49 +0200

    Output utility class skeleton

---
+++ CLI/SpectactularCLI/Utilities/Output.cs
@@ -0,0 +1,53 @@
+﻿using SpectactularCLI.Commands;
+
+namespace SpectactularCLI.Utilities
+{
+    public class Output
+    {
+        public static void PrintWelcomeBanner()
+        {
+            // Implement welcome banner printing
+        }
+
+        public static void PrintCommands()
+        {
+            Console.WriteLine();
+            foreach (Command command in Global.Commands)
+            {
+                if (!command.Hidden())
+                {
+                    Console.Write(command.GetPrefix());
+                    Console.WriteLine("\u001B[34m" + command.GetIdentifier() + "\u001B[0m" + " - " + command.GetDescription());
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintEnterCommand()
+        {
+            Console.Write("");
+        }
+
+        public static async Task PrintLoggedInUser()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", Global.AccessToken);
+
+                    HttpResponseMessage response = await client.GetAsync(Global.API_DOMAIN + "api/private/user");
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Handle exception
+            }
+        }
+    }
+}
