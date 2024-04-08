420be4f11ba41f86516cd2de2612c4b9a1f4a9f3
BBDNET\bbdnet2874 <mcquile@bbd.co.za>
2024-04-08 15:33:25 +0200

    Global class for development. Will modify configurations once final.

---
+++ CLI/SpectactularCLI/Utilities/Global.cs
@@ -0,0 +1,21 @@
+﻿using SpectactularCLI.Commands;
+
+namespace SpectactularCLI.Utilities
+{
+    public static class Global
+    {
+        public const string AUTH0_DOMAIN = "";
+        public const string AUTH0_CLIENT_ID = "";
+        public const string AUTH0_AUDIENCE = "";
+        public const string API_DOMAIN = "";
+        public static string AccessToken = "";
+        public static string User = "";
+
+        public static readonly List<Command> DefaultCommands = new List<Command>
+    {
+
+    };
+
+        public static List<Command> Commands = DefaultCommands;
+    }
+}
